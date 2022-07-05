4ee251ed4fe192c4113e564fd5684cca218decb6
Lanah Stanford <lanahstanford@gmail.com>
2022-07-05 14:10:35 -0400

    1. Makes a folder in Resources for consumables 2. Creates a potion log for testing purposes 3. Makes an folder for scripts related to items and moves all related files to that folder

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -6,14 +6,21 @@ public class InventoryController : MonoBehaviour
 {
 
     public PlayerWeaponController playerWeaponController;
+    public ConsumableController consumableController;
     public Item sword;
+    public Item staff;
+    public Item PotionLog;
 
     void Start()
     {
         playerWeaponController = GetComponent<PlayerWeaponController>();
+        consumableController = GetComponent<ConsumableController>();
         List<BaseStat> swordStats = new List<BaseStat>();
         swordStats.Add(new BaseStat(6, "Power", "Your power level"));
-        sword = new Item(swordStats, "fire_staff");
+        sword = new Item(swordStats, "sword");
+        staff = new Item(swordStats, "fire_staff");
+
+        PotionLog = new Item(new List<BaseStat>(), "potion_log", "Drink this to log something cool!", "Drink", "Log Potion", true);
     }
 
     void Update()
@@ -21,6 +28,11 @@ public class InventoryController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Z))
         {
             playerWeaponController.EquipWeapon(sword);
+            consumableController.ConsumeItem(PotionLog);
         }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            playerWeaponController.EquipWeapon(staff);           
+        }        
     }
 }
